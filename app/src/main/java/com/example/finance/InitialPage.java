24419cc507ea19efbd735c56475f67f394355e43
Théo Barreto Silva <barretotheo25@gmail.com>
2023-03-02 15:17:58 -0300

    upload

--- app/src/main/java/com/example/finance/InitialPage.java
+++ app/src/main/java/com/example/finance/InitialPage.java
@@ -3,9 +3,14 @@ package com.example.finance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class InitialPage extends AppCompatActivity {
+
+    TextView titulo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,20 @@ public class InitialPage extends AppCompatActivity {
         setContentView(R.layout.initial_page);
         getWindow().setStatusBarColor(Color.rgb(94,23,235));
         getSupportActionBar().hide();
+
+        titulo2 = findViewById(R.id.titulo2);
+
+        setarUnderline();
+    }
+
+    public void setarUnderline() {
+        if (Build.VERSION.SDK_INT >= 24)
+        {
+            titulo2.setText(Html.fromHtml("<u>O seu aplicativo favorito de finanças!</u>",Html.FROM_HTML_MODE_LEGACY));
+        }
+        else
+        {
+            titulo2.setText(Html.fromHtml("<u>O seu aplicativo favorito de finanças!</u>"));
+        }
     }
 }
